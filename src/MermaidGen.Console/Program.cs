c1326599947a5ef2d878ab7bf2959b0812a3fad1
Christof Sprenger <christof_sprenger@hotmail.com>
2025-06-26 20:19:10 -0700

    larger sample

--- src/MermaidGen.Console/Program.cs
+++ src/MermaidGen.Console/Program.cs
@@ -26,9 +26,33 @@ class Program
         out List<(string Id, string Label, string? Category)> nodes,
         out List<(string Source, string Target, string Type)> links)
     {
-        #pragma warning disable format
+#pragma warning disable format
         nodes =
         [
+            // Primordials
+            ("Uranus",     "Uranus - Sky Father",           "Primordials"),
+            ("Gaia",       "Gaia - Mother Earth",           "Primordials"),
+            ("Erebus",     "Erebus - God of Darkness",      "Primordials"),
+            ("Nyx",        "Nyx - Goddess of Night",        "Primordials"),
+            ("Tartarus",   "Tartarus - Deep Abyss",         "Primordials"),
+            ("Eros",       "Eros - God of Love",            "Primordials"),
+            ("Chaos",      "Chaos - Primordial Void",       "Primordials"),    
+            ("Hemera",     "Hemera - Goddess of Day",       "Primordials"),
+            ("Aether",     "Aether - God of Light",         "Primordials"),
+            ("Ananke",     "Ananke - Goddess of Necessity", "Primordials"),
+
+            // Titans 
+            ("Kronos",     "Kronos - Titan of Time",         "Titans"),
+            ("Rhea",       "Rhea - Mother of Gods",          "Titans"),
+            ("Oceanus",    "Oceanus - Titan of Ocean",       "Titans"),
+            ("Tethys",     "Tethys - Titaness of Rivers",    "Titans"),
+            ("Hyperion",   "Hyperion - Titan of Light",      "Titans"),
+            ("Theia",      "Theia - Titaness of Sight",      "Titans"),
+            ("Coeus",      "Coeus - Titan of Intelligence",  "Titans"),
+            ("Phoebe",     "Phoebe - Titaness of Prophecy",  "Titans"),
+            ("Crius",      "Crius - Titan of Constellations", "Titans"),
+            ("Iapetus",    "Iapetus - Titan of Mortality",   "Titans"),
+
             // Olympians
             ("Zeus",       "Zeus - King of Gods",           "Olympians"),
             ("Hera",       "Hera - Queen of Gods",          "Olympians"),
@@ -43,46 +67,56 @@ class Program
             ("Hermes",     "Hermes - Messenger God",        "Olympians"),
             ("Dionysus",   "Dionysus - God of Wine",        "Olympians"),
 
-            ("Kronos",     "Kronos - Titan of Time",        "Titans"),
-            ("Rhea",       "Rhea - Mother of Gods",         "Titans"),
-            ("Oceanus",    "Oceanus - Titan of Ocean",      "Titans"),
-            ("Tethys",     "Tethys - Titaness of Rivers",   "Titans"),
-
+         
+            // Heroes
             ("Heracles",   "Heracles/Hercules",             "Heroes"),
             ("Perseus",    "Perseus",                       "Heroes"),
             ("Theseus",    "Theseus",                       "Heroes"),
             ("Achilles",   "Achilles",                      "Heroes"),
-            
-            
         ];
-        #pragma warning restore format
+       
 
         links =
         [
             // Parent-Child
-            ("Kronos", "Zeus", "parent"),
-            ("Kronos", "Hera", "parent"),
-            ("Kronos", "Poseidon", "parent"),
-            ("Kronos", "Demeter", "parent"),
-            ("Rhea", "Zeus", "parent"),
-            ("Rhea", "Hera", "parent"),
-            ("Rhea", "Poseidon", "parent"),
-            ("Rhea", "Demeter", "parent"),
-            ("Zeus", "Athena", "parent"),
-            ("Zeus", "Apollo", "parent"),
-            ("Zeus", "Artemis", "parent"),
-            ("Zeus", "Ares", "parent"),
-            ("Zeus", "Hermes", "parent"),
-            ("Zeus", "Dionysus", "parent"),
-            ("Zeus", "Heracles", "parent"),
-            ("Zeus", "Perseus", "parent"),
-            ("Poseidon", "Theseus", "parent"),
-            ("Tethys", "Achilles", "ancestor"),
+            ("Kronos",     "Zeus",      "parent"),
+            ("Kronos",     "Hera",      "parent"),
+            ("Kronos",     "Poseidon",  "parent"),
+            ("Kronos",     "Demeter",   "parent"),
+            ("Rhea",       "Zeus",      "parent"),
+            ("Rhea",       "Hera",      "parent"),
+            ("Rhea",       "Poseidon",  "parent"),
+            ("Rhea",       "Demeter",   "parent"),
+            ("Zeus",       "Athena",    "parent"),
+            ("Zeus",       "Apollo",    "parent"),
+            ("Zeus",       "Artemis",   "parent"),
+            ("Zeus",       "Ares",      "parent"),
+            ("Zeus",       "Hermes",    "parent"),
+            ("Zeus",       "Dionysus",  "parent"),
+            ("Zeus",       "Heracles",  "parent"),
+            ("Zeus",       "Perseus",   "parent"),
+            ("Poseidon",   "Theseus",   "parent"),
+
+            // ancestors
+            // ("Uranus",     "Kronos",    "ancestor"),
+            // ("Gaia",       "Kronos",    "ancestor"),
+            // ("Erebus",     "Nyx",       "ancestor"),
+            // ("Nyx",        "Hemera",    "ancestor"),
+            // ("Hemera",     "Aether",    "ancestor"),
+            // ("Chaos",      "Ananke",    "ancestor"),
+            // ("Oceanus",    "Tethys",    "ancestor"),
+            // ("Hyperion",   "Theia",     "ancestor"),
+            // ("Coeus",      "Phoebe",    "ancestor"),
+            // ("Crius",      "Iapetus",   "ancestor"),
+            ("Tethys",     "Achilles",  "ancestor"),
+
             // Marriages
-            ("Zeus", "Hera", "married"),
-            ("Kronos", "Rhea", "married"),
-            ("Oceanus", "Tethys", "married"),
+            ("Zeus",       "Hera",      "married"),
+            ("Kronos",     "Rhea",      "married"),
+            ("Oceanus",    "Tethys",    "married"),
             ("Hephaestus", "Aphrodite", "married")
         ];
+#pragma warning restore format
+
     }
 }
